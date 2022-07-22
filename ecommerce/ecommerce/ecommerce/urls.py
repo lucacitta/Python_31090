72f82d8882393a9cedbfdf76d68b8116b13a0e3a
luca citta <lucacitta@gmail.com>
2022-07-21 22:28:37 -0300

    bucles y condicionales

--- ecommerce/ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/ecommerce/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from ecommerce.views import saludo, segundo_template
+from ecommerce.views import saludo, segundo_template, template_con_lista
 
 urlpatterns = [
     path('admin/', admin.site.urls),  
     path('saludo/', saludo, name='saludo'),
-    path('segundo_template/', segundo_template, name='segundo_template')
+    path('segundo_template/', segundo_template, name='segundo_template'),
+    path('template_con_lista/', template_con_lista, name='template_con_lista')
 ]
--- ecommerce/ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/ecommerce/views.py
@@ -14,3 +14,9 @@ def segundo_template(request):
     }
     return render(request, 'template_2.html', context=context)
 
+def template_con_lista(request):
+    context = {
+        'lista':['tomate', 'banana', 'naranja'],
+    }
+    return render(request, 'template_lista.html',context=context)
+
