0ca9f9e71d4e022aa92d4b3220d4e0f0110e082b
luca citta <lucacitta@gmail.com>
2022-07-28 21:52:50 -0300

    extends

--- ecommerce/ecommerce/products/views.py
+++ ecommerce/ecommerce/products/views.py
@@ -4,14 +4,18 @@ from products.models import Products
 
 
 def create_product(request):
-    new_product = Products.objects.create(name = 'Coca cola 1l', price = 350, stock = 10)
-    context = {
-        'new_product':new_product
-    }
+    productos = Products.objects.all()
+    context = {}
+    if len(productos) >= 3:
+        new_product = Products.objects.create(name = 'Coca cola 1l', price = 350, stock = 10)
+        context = {
+            'new_product':new_product
+        }
     return render(request, 'products/new_product.html', context=context)
 
 def list_products(request):
     products = Products.objects.all()
+    print(len(products))
     context = {
         'products':products
     }
