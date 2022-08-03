8e22fa42681801e4abbdc951425aefc0e3bbe02b
luca citta <lucacitta@gmail.com>
2022-08-02 23:21:16 -0300

    forms 6

--- ecommerce/ecommerce/products/views.py
+++ ecommerce/ecommerce/products/views.py
@@ -1,5 +1,3 @@
-from multiprocessing import context
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from products.models import Products
 from products.forms import Formulario_productos
@@ -41,6 +39,6 @@ def primer_formulario(request):
 
 def search_products(request):
     search = request.GET['search']
-    products = Products.objects.filter(name=search)
+    products = Products.objects.filter(name__icontains=search)
     context = {'products':products}
-    return HttpResponse(request.GET)
+    return render(request, 'products/search_product.html', context=context)
