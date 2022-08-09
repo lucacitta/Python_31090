2f2745af4ee6e8502619f8db81097492b79668e2
luca citta <lucacitta@gmail.com>
2022-08-09 20:33:56 -0300

    login

---
+++ ecommerce/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from users.views import login_request
+
+
+urlpatterns = [
+    path('login/', login_request, name='login'),
+]
---
+++ ecommerce/users/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                
+                context = {'message':f'Bienvenido {username}!! :D'}
+                return render(request, 'index.html', context = context)
+
+        form = AuthenticationForm()
+        return render(request, 'users/login.html', {'error': 'Formulário inválido', 'form': form})
+
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
