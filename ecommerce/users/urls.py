0a5c52af4068f4ace5bb7676ec338c202755c455
luca citta <lucacitta@gmail.com>
2022-08-09 21:22:08 -0300

    register

--- ecommerce/users/urls.py
+++ ecommerce/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import login_request
+from users.views import login_request, register
 
 
 urlpatterns = [
     path('login/', login_request, name='login'),
+    path('register/', register, name='register'),
 ]
--- ecommerce/users/views.py
+++ ecommerce/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 
+from users.forms import User_registration_form
+
 
 def login_request(request):
     if request.method == 'POST':
@@ -22,3 +24,14 @@ def login_request(request):
     else:
         form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = User_registration_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    else:
+        form = User_registration_form()
+    return render(request, 'users/register.html', {'form': form})
