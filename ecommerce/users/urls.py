19a0c63213134fb5553983ab7792c81859f56a67
luca citta <lucacitta@gmail.com>
2022-08-09 22:19:26 -0300

    login

--- ecommerce/users/urls.py
+++ ecommerce/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from users.views import login_request
 
 urlpatterns = [
-    
+    path('login/', login_request, name='login'),
 ]
--- ecommerce/users/views.py
+++ ecommerce/users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                
+                context = {'message':f'Bienvenido {username}!! :D'}
+                return render(request, 'index.html', context = context)
+
+        form = AuthenticationForm()
+        return render(request, 'users/login.html', {'error': 'Formulário inválido', 'form': form})
+
+    elif request.method == 'GET':
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
